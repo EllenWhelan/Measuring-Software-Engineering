dd8273fa25b2d2006428e3b465ad2adde07d94d4
Ellen Whelan <whelane5@tcd.ie>
2019-11-05 10:06:27 +0000

    Delete

--- GitInterrogation/GitInterrogation.py
+++ GitInterrogation/GitInterrogation.py
@@ -1,7 +1,7 @@
 import github3
 
-username = "EllenWhelan"
-password = "lamagh+mal_1998"
+username = "username"
+password = "password"
 
 userAccount = github3.login(username=username, password=password)
 if(userAccount.followers >1): print ('Hello world' )
