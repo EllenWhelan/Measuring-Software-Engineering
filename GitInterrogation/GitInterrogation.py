2597a620206232f580fa6e5f4ec1503843332a66
Ellen Whelan <whelane5@tcd.ie>
2019-11-05 10:38:46 +0000

    Listing users repos functionality

--- GitInterrogation/GitInterrogation.py
+++ GitInterrogation/GitInterrogation.py
@@ -1,7 +1,12 @@
 import github3
 
+
 username = "username"
 password = "password"
 
 userAccount = github3.login(username=username, password=password)
-if(userAccount.followers >1): print ('Hello world' )
+
+for repo in userAccount.repositories_by(username):
+    x=repo.git_url
+    print(x[18+len(username):-4])
+
