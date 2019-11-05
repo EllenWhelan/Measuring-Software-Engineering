37861d88181e23818f7e2f6c7f77aa3f7cf16e1c
Ellen Whelan <whelane5@tcd.ie>
2019-11-05 12:56:45 +0000

    Added functions

--- GitInterrogation/GitInterrogation.py
+++ GitInterrogation/GitInterrogation.py
@@ -1,12 +1,32 @@
 import github3
+#globals
+myRepos= []
 
-
+# login stuff
 username = "username"
 password = "password"
-
 userAccount = github3.login(username=username, password=password)
 
-for repo in userAccount.repositories_by(username):
-    x=repo.git_url
-    print(x[18+len(username):-4])
 
+def listingRepos (username):
+    # listing users repos
+    i = 1
+    for repo in userAccount.repositories_by(username):
+        x = repo.git_url
+        myRepos.append(repo)
+        print(str(i) + '.' + x[18 + len(username):-4])
+        i = i + 1
+    print('Please enter the number of the repo you would like to view.')
+    repoNum = input()
+    detailingRepo(repoNum)
+
+def detailingRepo (repoNum):
+    # displaying repo details
+    print(myRepos[repoNum - 1].git_url)
+    print(myRepos[repoNum-1].)
+    print ('Enter 1 to exit')
+    if input() == 1 :listingRepos(username)
+
+
+
+listingRepos(username)
